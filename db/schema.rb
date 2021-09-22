498d049a5cc367366314f2d1b906b44f84d51603
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-23 00:22:29 +0300

    edit styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_162219) do
+ActiveRecord::Schema.define(version: 2021_08_29_035032) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "excerpt"
+    t.string "location"
   end
 
 end
--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "articles/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "articles/index", type: :view do
+  before(:each) do
+    assign(:articles, [
+      Article.create!(
+        title: "Title",
+        body: "MyText"
+      ),
+      Article.create!(
+        title: "Title",
+        body: "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of articles" do
+    render
+    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: "MyText".to_s, count: 2
+  end
 end
