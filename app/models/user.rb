4a3974fe5b0caa2d657aef141ff5990c5487352c
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-25 11:42:34 +0300

    Add password validation for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
                     length: { in: 5..50 }
                     format: { with: /\A[^@][\w.-]+@[\w.-]+[.][az]{2,4}\z/i }
-  
+  validates :password, confirmation: true
+              
 
   has_one :profile
   has_many :articles, -> { order 'published_at DESC, title ASC' }, 
