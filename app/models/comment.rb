11d347051b4b7158f9502fe21811a3bbcb29ef71
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-26 01:19:28 +0300

    Add a callback to the model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,8 @@ class Comment < ApplicationRecord
   def article_should_be_published
     errors.add(:article_id, 'is not published yet') if article && !article.published?
   end
+
+  def email_article_author
+    puts 'We will notify the #{article.user.email} in Chapter 12' if article.user
+  end
 end
