8c61d8bccba8ca0afe796b7a44f443e2c0871d1f
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-25 04:08:02 +0300

    Create a comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_153630) do
+ActiveRecord::Schema.define(version: 2021_09_25_010708) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2021_09_24_153630) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "name"
+    t.string "email"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
