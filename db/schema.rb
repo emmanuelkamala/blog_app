d23963c0b772e84b98e75c5a2857adbf29d3076d
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-03-21 16:54:58 +0300

    Create a rich joi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_21_125532) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_21_131417) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "excerpt"
@@ -32,6 +32,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_21_125532) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "name"
+    t.string "email"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
