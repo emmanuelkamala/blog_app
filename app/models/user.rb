f92c53057d7a89a2b288e8d60ae9513c4e2c599f
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-03-21 17:28:47 +0300

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true 
+  validates :email, length: { in: 5..50 }
+  validates :email, format: { with: /\A[^@][\w.-]+@[\w.-]+[.][az]{2,4}\z/i }
+  validates :password, confirmation: true
+
   has_one :profile
   has_many :articles, -> { order 'published_at DESC, title ASC' }, dependent: :nullify
   has_many :replies, through: :articles, source: :comments
