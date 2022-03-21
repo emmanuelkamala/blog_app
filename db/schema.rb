ac7e4d00ffa9762c5ee299eeea8c9c917577af6d
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-03-21 14:35:48 +0300

    Create a profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_17_204314) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_21_105445) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "excerpt"
@@ -21,4 +21,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_204314) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.date "birthday"
+    t.text "bio"
+    t.string "color"
+    t.string "twitter"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "profiles", "users"
 end
