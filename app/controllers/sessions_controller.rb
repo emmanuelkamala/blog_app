998ef00ab2e9514d9668dcf27b4cff29fb96789e
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-07-04 21:18:38 +0300

    Edit article

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  def create
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid login/password combination"
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
+  resource :session
+  get "/login", to: "sessions#new", as: "login"
+  get "/logout", to: "sessions#destroy", as: "logout" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
